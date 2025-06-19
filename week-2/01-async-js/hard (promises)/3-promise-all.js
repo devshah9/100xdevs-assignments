68b3a7752d419dcaf2f3dcc252a7bec4a5f960df
Dev Shah <devshah972@gmail.com>
2025-06-19 20:59:25 +0530

    Completed Week 2 assginment

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,29 @@
  */
 
 function wait1(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(resolve, t);
+  });
 }
 
 function wait2(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(resolve, t);
+  });
 }
 
 function wait3(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(resolve, t);
+  });
 }
 
 function calculateTime(t1, t2, t3) {
-
+  const now = Date.now();
+  return Promise.all([wait1(t1*1000), wait2(t2*1000), wait3(t3*1000)]).then(() => {
+    const end = Date.now();
+    return end - now;
+  });
 }
 
 module.exports = calculateTime;
